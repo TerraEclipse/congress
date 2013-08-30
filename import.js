8cd08e5c78a5eca1edba8acd0b7891446a42386a
Carlos Rodriguez <carlos@s8f.org>
2013-08-30 14:20:30 -0700

    implement simple cache

--- import.js
+++ import.js
@@ -3,19 +3,41 @@ var db = require('level')('./db', {valueEncoding: 'json'});
 console.log('searching for data files...');
 var latch = 1, errored = false;
 
-require('glob')('**/data.json')
+var base = './' + (process.argv[2] && process.argv[2] + '/' || '');
+
+require('glob')(base + '**/data.json')
   .on('match', function (p) {
-    try {
-      var data = require('./' + p);
+    var match = p.match(/data\/(\d+)\/(amendments|bills)\/([^\/]+)\/([^\/]+)\//);
+    if (match) return tryGet(match[3] + '-' match[1]);
+    var match = p.match(/data\/(\d+)\/votes\/(\d+)\/([^\/]+)\//);
+    if (match) return tryGet(match[3] + '-' + match[1] + '.' + match[2]);
+    console.error('no match', p);
+    tryEnd();
+
+    function tryGet (id) {
+      db.get(id, function (err, data) {
+        if (err) return tryEnd(err);
+        if (!data) put();
+        else {
+          console.log(id, 'already in db');
+          tryEnd();
+        }
+      });
     }
-    catch (e) {
-      return console.error(e);
+
+    function put () {
+      try {
+        var data = require(base + p);
+      }
+      catch (e) {
+        return console.error(e);
+      }
+      var id = data.bill_id || data.amendment_id || data.vote_id;
+      if (!id) return console.error('no id for ' + p);
+      latch++;
+      console.log('putting', id);
+      db.put(id, data, tryEnd);
     }
-    var id = data.bill_id || data.amendment_id || data.vote_id;
-    if (!id) return console.error('no id for ' + p);
-    latch++;
-    console.log('putting', id);
-    db.put(id, data, tryEnd);
   })
   .on('end', function () {
     tryEnd();
