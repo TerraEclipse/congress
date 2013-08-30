2b663084471b1bae4d2aa63629c0fb58c223f29a
Carlos Rodriguez <carlos@s8f.org>
2013-08-30 11:32:02 -0700

    import into leveldb

--- import.js
+++ import.js
@@ -1,11 +1,33 @@
-var db = require('level')('./db');
+var db = require('level')('./db', {valueEncoding: 'json'});
+
+console.log('searching for data files...');
+var latch = 1, errored = false;
 
 require('glob')('**/data.json')
   .on('match', function (p) {
-    console.log(p);
+    try {
+      var data = require(p);
+    }
+    catch (e) {
+      return console.error(e);
+    }
+    var id = data.bill_id || data.amendment_id || data.vote_id;
+    if (!id) return console.error('no id for ' + p);
+    latch++;
+    console.log('putting', id);
+    db.put(id, data, tryEnd);
   })
-  .on('end', function () {
+  .on('end', tryEnd)
+
+function tryEnd (err) {
+  if (errored) return;
+  if (err) {
+    errored = true;
+    return console.error(err);
+  }
+  if (!--latch) {
     db.close(function () {
-      console.log('done');
+      console.log('done.');
     });
-  })
+  }
+}
